646adf6cd9e2754b62292e6447fa972cda88aefa
Belalia Mohamed Alaa Eddine <mascaprod7@gmail.com>
2020-03-26 16:02:33 +0100

    al2a 1602.26032020

--- send_emails.php
+++ send_emails.php
@@ -5,16 +5,16 @@ if(isset($_POST['submit'])){
     $message=$_POST['message'];
 
     if(mail($email,$subject,$message)){
-        echo "mail send";
+        $alert=1;
     }else{
-        echo "failed";
+      $alert=2;
     }
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Bootstrap Example</title>
+  <title>Email Sender</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
@@ -25,7 +25,24 @@ if(isset($_POST['submit'])){
 <body>
 
 <div class="container">
-  <h2>Stacked form</h2>
+  <h2>Email Sender</h2>
+  <?php
+  if(isset($alert)){
+    if($alert==1){
+  ?>
+  <div class="alert alert-success">
+    <strong>Success!</strong> mail send
+  </div>
+  <?php
+    }else{
+      ?>
+      <div class="alert alert-warning">
+        <strong>Success!</strong> failed
+      </div>
+      <?php
+    }
+  }
+  ?>
   <form action="send_emails.php" method="post">
     <div class="form-group">
       <label for="email">To Email:</label>
@@ -39,7 +56,7 @@ if(isset($_POST['submit'])){
       <label for="message">Message:</label>
       <input type="text" class="form-control" id="message" placeholder="Enter message" name="message">
     </div>
-    <button type="submit" name="submit" class="btn btn-primary">send</button>
+    <button type="submit" name="submit" class="btn btn-primary btn-block">Send</button>
   </form>
 </div>
 
