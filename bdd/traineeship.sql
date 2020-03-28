3655016957d9c3b09470d4058775fdb3aa94ea30
Belalia Mohamed Alaa Eddine <mascaprod7@gmail.com>
2020-03-28 10:52:54 +0100

    al2a 1052.28032020

--- bdd/traineeship.sql
+++ bdd/traineeship.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 27 mars 2020 à 19:37
+-- Généré le : sam. 28 mars 2020 à 10:50
 -- Version du serveur :  10.4.11-MariaDB
 -- Version de PHP : 7.4.3
 
@@ -52,6 +52,56 @@ INSERT INTO `admin` (`id`, `sexe`, `firstname`, `lastname`, `n_cni`, `adr`, `pho
 (1, 'Homme', 'Belalia', 'Mohamed Alaa Eddine', 123456789, 'rue ould ben khadda mohamed', 697701228, 'mascaprod7@gmail.com', '68053af2923e00204c3ca7c6a3150cf7', 'ZvO$yYXJtL', 1, 0, '2020-03-27'),
 (6, 'Homme', 'mohamed', 'alaa eddine', 123, 'mascara', 123, 'belalia.alaaeddine@gmail.com', '202cb962ac59075b964b07152d234b70', ')67wdieL0U', 1, 1, '2020-03-27');
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `sexe` varchar(255) NOT NULL,
+  `dn` date NOT NULL,
+  `n_cni` int(11) NOT NULL,
+  `eta` varchar(255) NOT NULL,
+  `adr` varchar(255) NOT NULL,
+  `nv_etd` varchar(255) NOT NULL,
+  `Specialite` varchar(255) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `isEmailConfirmed` int(11) DEFAULT 0,
+  `archived` int(11) NOT NULL DEFAULT 0,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `entreprise`
+--
+
+CREATE TABLE `entreprise` (
+  `id` int(11) NOT NULL,
+  `n_serie` varchar(255) NOT NULL,
+  `denomination` varchar(255) NOT NULL,
+  `nom_dirigeant` varchar(255) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `siege_social` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `secteur_act` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `isEmailConfirmed` int(11) NOT NULL DEFAULT 0,
+  `archived` int(11) NOT NULL DEFAULT 0,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 --
 -- Index pour les tables déchargées
 --
@@ -63,6 +113,18 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Index pour la table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `entreprise`
+--
+ALTER TABLE `entreprise`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
@@ -71,6 +133,18 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT pour la table `client`
+--
+ALTER TABLE `client`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `entreprise`
+--
+ALTER TABLE `entreprise`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
