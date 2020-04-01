408c1ddcc4d64ae058f7824a7fd576381a7d0ea9
unknown <souad.beguenane@gmail.com>
2020-04-02 00:21:34 +0200

    done

--- update_client.php
+++ update_client.php
@@ -53,7 +53,7 @@ if (isset($_GET['id'])) {
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Modifier un admin</title>
+  <title>Modifier un client</title>
 
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
